7f70c871e285a3cae633230471cc2237f9448fc1
Rene Dellefont <renedellefont@gmail.com>
2016-10-18 00:47:59 -0400

    Add masonry options

--- _assets/scripts/_shows.js
+++ _assets/scripts/_shows.js
@@ -2,6 +2,4 @@
 var $ = require('jquery');
 var m = require('masonry-layout');
 
-var msnry = new m( '.shows__container', {
-  
-});
+var msnry = new m( '.shows__container', {itemSelector: '.show', columnWidth: '.show'});
