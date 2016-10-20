6f2f735280457befff4dfdea4c93e254a81d6083
Rene Dellefont <renedellefont@gmail.com>
2016-10-20 14:53:07 -0400

    Wait to call Masonry until images loaded

--- _assets/scripts/_shows.js
+++ _assets/scripts/_shows.js
@@ -1,5 +1,10 @@
 'use strict';
 var $ = require('jquery');
 var m = require('masonry-layout');
+var il = require('imagesloaded');
 
-var msnry = new m( '.shows__container', {itemSelector: '.show', columnWidth: '.show'});
+var grid = $('.shows__container').imagesLoaded( function() {
+  console.log('imagesLoaded called');
+  // init Masonry after all images have loaded
+  new m( '.shows__container', {itemSelector: '.show', columnWidth: '.show'});
+});
