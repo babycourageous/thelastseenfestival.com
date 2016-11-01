ca7361a05b8ce934970558985ab132bd83777a4d
Rene Dellefont <renedellefont@gmail.com>
2016-11-01 12:31:04 -0400

    Update show magnific call to inline

--- _assets/scripts/_magnific.js
+++ _assets/scripts/_magnific.js
@@ -5,5 +5,5 @@ require('magnific-popup');
 $(document).ready(function() {
   $('.workshop__link').magnificPopup({type:'ajax'});
   $('.schedule__link').magnificPopup({type:'ajax'});
-  $('.show__link').magnificPopup({type:'ajax'});
+  $('.show__link').magnificPopup({type:'inline',midClick: true});
 });
